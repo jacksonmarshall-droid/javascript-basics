e3889486308924ed18e374da0f670ae0c404312a
jacksonmarshall-droid <jackson.marshall@codedbykids.org>
2026-04-22 22:59:22 +0000

    JUDGEMENT pt 2

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
 let name = "Jackson";
 console.log(name);
+let myage ="13";
+let islearningJS=true
+let cost = 0
+let funds = 15
+let item1 = "cheeseburger"
+let price1 = 12.50
+let item2 = "french fries"
+let price2 = 5.00
+let item3 = "buttery Maine Lobster" 
+let price3 = 30.00
+let item4 = "white rice with Beef,lobster,lettuse"
+let price = 35.00
+
+// order for fries and lobster
+price = price3 + price2
+console.log(price)
+// 35.00 
+// funds 20.00
